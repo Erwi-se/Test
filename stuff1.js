620f20c4ab09989d690f9abf91f2dfaf6ea20e0a
Erwi <captainspideriron@gmail.com>
2025-01-15 23:26:58 +0700

    Update stuff1.js

--- stuff1.js
+++ stuff1.js
@@ -1,20 +1,33 @@
-function getCard() {
-  let arr = [...Array(22).keys()] 
+addEventListener("DOMContentLoaded", function() {
+slider = document.getElementById("cardRange");
+output = document.getElementById("demo");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+});
+
+function getCard(slider) {
+  let arr = [...Array(22).keys()];
   let result = [];
-  let id = [1, 2, 3, 4] 
-  
-  for (let i = 1; i <= 4; i++) {
+  let id = [...Array(slider.value).keys()].map(i => i+1);
+  console.log(id)
+  for (let i = 1; i <= slider.value; i++) {
     const random = Math.floor(Math.random() * (22- i));
     result.push(arr[random]);
     arr[random] = arr[22 - i];
   }
     
-  for (let i = 0; i < 4; i++) {
+  for (let i = 0; i < slider.value; i++) {
     document.getElementById(id[i]).src = result[i] + "_tarot.png";
   }
   console.log(result)
 }
 
-function getDes(id) {
-  document.getElementById("des").innerHTML = id;
+function getDes(url) {
+  let src = url.match(/\d+(?=_)/g);
+  if (src == 0) {
+  document.getElementById("des").innerHTML = "The Fool";
+  }
 }
